4d03574686d0aa808b87c6ab6e9d66e8d180f070
Jean-luc Kashindi Nestor <jeanluckashindipro@gmail.com>
2026-04-16 23:53:38 +0200

    fix: correct deployment path to public_html/2starshop/backend

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Chemin du projet sur le serveur
-PROJECT_DIR = '/home/neyba/backend'
+PROJECT_DIR = '/home/neyba/public_html/2starshop/backend'
 sys.path.insert(0, PROJECT_DIR)
 
 # Définir le module de settings Django
