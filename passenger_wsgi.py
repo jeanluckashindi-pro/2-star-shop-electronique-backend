ee4ad2907043f062cfefc2284d751ebb143d307b
Jean-luc Kashindi Nestor <jeanluckashindipro@gmail.com>
2026-04-16 23:21:01 +0200

    updates

---
+++ passenger_wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+# Chemin du projet sur le serveur
+PROJECT_DIR = '/home/neyba/backend'
+sys.path.insert(0, PROJECT_DIR)
+
+# Définir le module de settings Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Importer l'application WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
